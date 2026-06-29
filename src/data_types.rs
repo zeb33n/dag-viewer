75f6d3e25529f5b704a812dc1f8eb299515cbd80
zeb <mrzebsta@gmail.com>
2026-06-29 14:18:53 +0100

    refactored to unify on one node type (removed logical nodes)

--- src/data_types.rs
+++ src/data_types.rs
@@ -2,8 +2,66 @@ use std::ops::{self, AddAssign, SubAssign};
 // rgba
 pub type Colour = u32;
 
+#[derive(Clone)]
+pub struct Line {
+    pub a: VecF2,
+    pub b: VecF2,
+    pub colour: Colour,
+}
 
 #[derive(Clone)]
+
+pub struct Path {
+    pub from: NodeHandle,
+    pub to: NodeHandle,
+    pub line_segments: Vec<Line>,
+}
+
+impl Path {
+    pub fn new(to: NodeHandle, from: NodeHandle) -> Self {
+        Self {
+            from: from,
+            to: to,
+            line_segments: vec![],
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Node {
+    pub is_fake_node: bool,
+    pub position: VecF2,
+    pub colour: Colour,
+    pub edges: Vec<usize>,
+    pub label: String,
+    pub dependents: Vec<usize>,
+}
+
+impl Node {
+    pub fn new(label: String) -> Self {
+        Self {
+            is_fake_node: false,
+            position: VecF2 { x: 0.0, y: 0.0 },
+            colour: 0xFF000055,
+            edges: vec![],
+            label: label,
+            dependents: vec![],
+        }
+    }
+
+    pub fn new_fake_node() -> Self {
+        Self {
+            is_fake_node: true,
+            position: VecF2 { x: 0.0, y: 0.0 },
+            colour: 0x00000000,
+            edges: vec![],
+            label: "".to_string(),
+            dependents: vec![],
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
 pub struct VecF2 {
     pub x: f32,
     pub y: f32,
@@ -49,7 +107,7 @@ impl AddAssign for VecF2 {
 
 impl ops::Sub<VecF2> for VecF2 {
     type Output = Self;
-    
+
     fn sub(self, rhs: Self) -> Self::Output {
         Self {
             x: self.x - rhs.x,
@@ -84,10 +142,9 @@ impl ops::MulAssign<f32> for VecF2 {
 
 impl VecF2 {
     pub fn dot(&self, rhs: &VecF2) -> f32 {
-        self.x * rhs.x + self.y * rhs.y 
+        self.x * rhs.x + self.y * rhs.y
     }
 }
-
 
 impl VecI2 {
     pub fn from_vecf2(vec: VecF2) -> Self {
@@ -105,10 +162,9 @@ impl VecI2 {
     }
 }
 
-
 pub struct Circle {
     pub center: VecF2,
     pub radius: f32,
 }
 
-pub type LogicalNodeHandle = usize;
+pub type NodeHandle = usize;
